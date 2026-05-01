d3dd7c9b8b5354f31ca9fb6584cc5ca4448cc920
ChudoWindows <chudo.windows@gmail.com>
2026-05-01 20:51:54 +0700

    Обновлен скрипт build.sh

--- build.sh
+++ build.sh
@@ -3,40 +3,87 @@ set -e
 
 project_name="FlyIC.Kernel.MemFunc"
 
-echo "Сборка пакета $project_name"
-echo "Для работы необходим компилятор clang и линкер llvm"
-echo ""
-
 mkdir -p build
 mkdir -p lib
 
-echo "Введите тип сборки:"
-read build_type
+# Инициализация переменных
+build_type=""
+hide=0
 
-if [ $build_type != "Debug" ] && [ $build_type != "Release" ]; then
-	echo "Не верный тип сборки ($build_type)"
-	echo "Будет использован тип сборки \"Debug\""
-	build_type="Debug"
+# Разбор аргументов командной строки
+while [ $# -gt 0 ]; do
+    case "$1" in
+        --build-type|-b)
+            build_type="$2"
+            shift 2
+            ;;
+        --hide|-h)
+            hide=1
+            shift
+            ;;
+        *)
+            shift
+            ;;
+    esac
+done
+
+if [ $hide -eq 0 ]; then
+	echo "Сборка модуля $project_name"
+	echo "Для работы необходимо:"
+	echo "  * Компилятор clang"
+	echo "  * Линковщик llvm"
+	echo ""
 fi
 
-mkdir -p build/$build_type
-mkdir -p lib/$build_type
+# Если тип сборки не указан, запрашиваем
+if [ -z "$build_type" ]; then
+    echo "Введите тип сборки:"
+    read build_type
+fi
+
+# Проверка и установка типа сборки
+if [ "$build_type" != "Debug" ] && [ "$build_type" != "Release" ]; then
+    echo "Не верный тип сборки ($build_type)"
+    echo "Будет использован тип сборки \"Debug\""
+    build_type="Debug"
+fi
+
+mkdir -p "build/$build_type"
+mkdir -p "lib/$build_type"
 
 compile_flags="-Iinclude -IFlyIC.Kernel.Base/include -nostdlib -ffreestanding -fno-rtti -fno-exceptions -fno-stack-protector"
 
-if [ $build_type == "Debug" ]; then
-	compile_flags="$compile_flags -g -O0"
+if [ "$build_type" = "Debug" ]; then
+    compile_flags="$compile_flags -g -O0"
+elif [ "$build_type" = "Release" ]; then
+    compile_flags="$compile_flags -O2 -march=native"
 fi
 
-if [ $build_type == "Release" ]; then
-	compile_flags="$compile_flags -O2 -march=native"
+log() {
+    if [ "$hide" -eq 0 ]; then
+        echo "($1/$2) $3"
+    fi
+}
+
+log 1 3 "FlyIC.Kernel.Base"
+
+cd FlyIC.Kernel.Base
+./build.sh -b $build_type -h
+
+cd ..
+
+
+log 2 3 "lib.cpp"
+clang++ -c ./src/lib.cpp -o "./build/$build_type/lib.o" $compile_flags
+
+log 3 3 "Archiving..."
+llvm-ar rcs "./lib/$build_type/$build_type.$project_name.a" \
+    "./build/$build_type/lib.o"
+
+# Если не указан флаг --hide, ждём нажатия Enter
+if [ "$hide" -eq 0 ]; then
+    echo ""
+    echo "Успешно!"
+    echo "Для выхода нажмите Enter..."
+    read
 fi
-
-echo "(1/2) lib.cpp"; clang++ -c ./src/lib.cpp -o ./build/$build_type/lib.o $compile_flags
-echo "(2/2) Archiving..."; llvm-ar rcs ./lib/$build_type/$build_type.$project_name.a ./build/$build_type/lib.o
-
-echo ""
-echo "Успешно!"
-echo "Для выхода нажмите Enter..."
-
-read
